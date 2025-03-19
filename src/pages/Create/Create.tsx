50b8ee8d591fffdb4c448d6e30e82222b6fca3b3
JoshuaW13 <josh.walters@shaw.ca>
2025-03-18 22:35:23 -0600

    Got cycle through quiz buttons except delete

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,12 +1,78 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./Create.module.css";
+import { Plus, Trash2, ArrowRight, ArrowLeft } from "lucide-react";
 import ImageUpload from "../../components/ImageUpload";
+
+export type Question = {
+  imageFile: File | null;
+};
 
 export default function Create() {
   const [quizName, setQuizName] = useState("");
   const [quizTheme, setQuizTheme] = useState("");
   const [quizType, setQuizType] = useState("");
-  const [previewImage, setPreviewImage] = useState<File | null>(null);
+  const previewImage = useRef<File | null>(null);
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  const [inputImage, setInputImage] = useState<File | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
+
+  const saveQuestions = () => {
+    if (currentIndex === -1) {
+      console.log("saving preview image");
+      previewImage.current = inputImage;
+      return;
+    }
+    let questionToAdd: Question = {
+      imageFile: inputImage,
+    };
+    console.log(
+      "saving the question" + questionToAdd + " at index " + currentIndex
+    );
+    setQuestions((prevQuestions) => {
+      let newQuestions = [...prevQuestions];
+      newQuestions[currentIndex] = questionToAdd;
+      return newQuestions;
+    });
+  };
+
+  const addQuestion = () => {
+    saveQuestions();
+    if (currentIndex == -1) {
+      setCurrentIndex((prevCurrentIndex) => prevCurrentIndex + 1);
+    } else {
+      setCurrentIndex((prevCurrentIndex) => questions.length);
+    }
+    setInputImage(null);
+  };
+
+  const onBack = () => {
+    console.log(questions);
+    const newIndex = currentIndex - 1;
+    saveQuestions();
+    if (newIndex == -1) {
+      setInputImage(previewImage.current);
+    } else {
+      console.log("trying to acccess index " + newIndex);
+      setInputImage(questions[newIndex].imageFile);
+    }
+    setCurrentIndex(newIndex);
+  };
+
+  const onNext = () => {
+    console.log(questions);
+    saveQuestions();
+    setCurrentIndex((prevIndex) => {
+      const newIndex = prevIndex + 1;
+      setInputImage(questions[newIndex]?.imageFile || null);
+      return newIndex;
+    });
+  };
+
   return (
     <div className={styles.quizDetails}>
       <div className={styles.topContainer}>
@@ -19,12 +85,15 @@ export default function Create() {
         value={quizName}
         onChange={(e) => setQuizName(e.target.value)}
       />
-      <input
-        type="text"
-        placeholder={"Quiz Theme"}
+      <select
+        id="quizTheme"
         value={quizTheme}
-        onChange={(e) => setQuizTheme(e.target.value)}
-      />
+        onChange={(e) => setQuizType(e.target.value)}
+      >
+        <option value="">Quiz Theme</option>
+        <option value="blur">Need to add these still</option>
+        <option value="zoom">Animals</option>
+      </select>
       <select
         id="quizType"
         value={quizType}
@@ -34,7 +103,37 @@ export default function Create() {
         <option value="blur">image blur</option>
         <option value="zoom">image zoom</option>
       </select>
-      <ImageUpload file={previewImage} setFile={setPreviewImage}></ImageUpload>
+      <h2>
+        {currentIndex > -1
+          ? `Question ${currentIndex} and ${questions.length}`
+          : "Preview Image"}
+      </h2>
+
+      <div className={styles.imageContainer}>
+        <div className={styles.questionButtonContainer}>
+          {currentIndex > -1 && (
+            <button className={styles.questionButtons}>
+              <Trash2 />
+            </button>
+          )}
+          {currentIndex > -1 && (
+            <button className={styles.questionButtons} onClick={onBack}>
+              <ArrowLeft />
+            </button>
+          )}
+        </div>
+        <ImageUpload file={inputImage} setFile={setInputImage}></ImageUpload>
+        <div className={styles.questionButtonContainer}>
+          <button className={styles.questionButtons} onClick={addQuestion}>
+            <Plus />
+          </button>
+          {questions.length > 0 && currentIndex < questions.length - 1 && (
+            <button className={styles.questionButtons} onClick={onNext}>
+              <ArrowRight />
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
