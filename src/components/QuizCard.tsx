b2c69e6db30906f20128b70a7324132871951fd7
Austin Lee <austinlee011@gmail.com>
2025-04-05 20:00:59 -0600

    feat: Added a favorite button on quiz cards which appears when signed in

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,8 +1,20 @@
 import { Quiz } from "../util/appwrite";
 import styles from "./QuizCard.module.css";
 import { Link } from "react-router-dom";
+import { Star } from "lucide-react";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function QuizCard({ quiz }: { quiz: Quiz }) {
+  const { user } = useAuth();
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const toggleFavorite = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsFavorited(!isFavorited);
+    //Make this actually update list of users favorited quizzes
+  };
+
   return (
     <Link
       key={quiz.id}
@@ -15,6 +27,15 @@ export default function QuizCard({ quiz }: { quiz: Quiz }) {
           alt={`Image for ${quiz.title}`}
           className={styles.quizImage}
         />
+        {user && (
+          <button className={styles.favoriteButton} onClick={toggleFavorite}>
+            <Star
+              id="favoriteIcon"
+              fill={isFavorited ? "gold" : "white"}
+              stroke="black"
+            />
+          </button>
+        )}
       </div>
       <h4 className={styles.quizGenre}>{quiz.theme}</h4>
       <h3 className={styles.quizTitle}>{quiz.title}</h3>
