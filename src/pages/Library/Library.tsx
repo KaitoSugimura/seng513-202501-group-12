1a5f95b24ce4ac55b22b5240e8233872b48df6b9
Austin Lee <austinlee011@gmail.com>
2025-04-06 19:38:35 -0600

    feat: Added Favorited Quizzes or Quiz History selectable tabs to the library page

--- src/pages/Library/Library.tsx
+++ src/pages/Library/Library.tsx
@@ -3,24 +3,28 @@ import QuizCard from "../../components/QuizCard";
 import { useAuth } from "../../context/AuthContext";
 import { databases, dbId, Quiz } from "../../util/appwrite";
 import styles from "./Library.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function Library() {
   const { user, loadingAuth } = useAuth();
   const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+  const [activeTab, setActiveTab] = useState<
+    "favoritedQuizzes" | "quizHistory"
+  >("favoritedQuizzes");
 
   useEffect(() => {
     if (!user) {
       return;
     }
 
-    const fetchQuizzes = async () => {
+    const fetchFavoriteQuizzes = async () => {
       const quizzes = (await databases.listDocuments(dbId, "quizzes"))
         .documents as Quiz[];
 
       setQuizzes(quizzes);
     };
 
-    fetchQuizzes();
+    fetchFavoriteQuizzes();
   }, [user]);
 
   if (loadingAuth) {
@@ -37,11 +41,42 @@ export default function Library() {
     <div className={styles.libraryRoot}>
       <h1 className={styles.title}>Library</h1>
 
-      <div className={styles.quizGrid}>
-        {quizzes.map((quiz) => (
-          <QuizCard key={quiz.$id} quiz={quiz} />
-        ))}
-      </div>
+      {!user && (
+        <>
+          <h3>Sign in to access library features.</h3>
+
+          <NavLink to="/account">
+            <button>Sign In</button>
+          </NavLink>
+        </>
+      )}
+      {user && (
+        <>
+          <div className={styles.libraryTabLayout}>
+            <button
+              className={`${styles.tab} ${
+                activeTab === "favoritedQuizzes" ? styles.selected : ""
+              }`}
+              onClick={() => setActiveTab("favoritedQuizzes")}
+            >
+              Favorited Quizzes
+            </button>
+            <button
+              className={`${styles.tab} ${
+                activeTab === "quizHistory" ? styles.selected : ""
+              }`}
+              onClick={() => setActiveTab("quizHistory")}
+            >
+              Quiz History
+            </button>
+          </div>
+          <div className={styles.quizGrid}>
+            {quizzes.map((quiz) => (
+              <QuizCard key={quiz.$id} quiz={quiz} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
